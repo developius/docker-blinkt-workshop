3533ee5b999b521423c74875eddf1e12f67c6038
Finnian Anderson <finnian.anderson@gmail.com>
2017-04-17 15:00:41 -0500

    Add safety catch to auto config script

--- auto_configure.sh
+++ auto_configure.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+echo 'Please _please_ make sure you have adjusted the SD volume location & the disk ID'
+echo 'Sleeping for 5s for safety'
+sleep 5
+
 touch /Volumes/boot/ssh
 cp ./config.txt /Volumes/boot/config.txt
 cp ./cmdline.txt /Volumes/boot/cmdline.txt
